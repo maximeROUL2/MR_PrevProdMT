af9f1e4542f890d7924adae0276e47fb2b5ed2b8
maxime.roul <maxime.roul@enercoop.org>
2020-05-26 16:49:36 +0200

    Ecriture en base (INSERT INTO) ajouter à la ConnexionDatabase.py

--- ConnexionDatabase.py
+++ ConnexionDatabase.py
@@ -1,6 +1,7 @@
 import keyring
 import pandas
 from sqlalchemy import create_engine
+import psycopg2
 
 
 """ 
@@ -8,10 +9,12 @@ ENTREE : une requête SQL et la connexion à la BDD d'enercoop du pôle énérgi
         
 HYPOTHESES : Les mots de passe sont cryptées par la méthode keyring (https://pypi.org/project/keyring/)
              On souhaite avoir un dataFrame en sortie pour les traitements futur
+             Les formats d'écriture dans la base de données doivent être respecter en amont 
              
 SORTIES : pandas_sql : le data frame lié à la requête 
          pandas_sql_chunk : un dataframe par morceau pour un traitement optimisé
          read_csv : la lecture d'un csv à partir du chemin et du fichier
+         write_database : inserer une data_frame dans une table associé 
 """
 
 class ConnexionDatabase:
@@ -22,6 +25,7 @@ class ConnexionDatabase:
         self.user = keyring.get_password("system", "username")
         self.pwd = keyring.get_password("system", "password")
         self.port = 5432
+        self.connection = psycopg2.connect(host=self.host, user=self.user, password=self.pwd, dbname=self.database)
 
     def pandas_sql(self, query):
         engine = create_engine("postgresql://{}:{}@{}:{}/{}".format(self.user, self.pwd, self.host, self.port,
@@ -45,10 +49,23 @@ class ConnexionDatabase:
 
         return data
 
+    def write_database(self, dataframe, table):
+        cursor = self.connection.cursor()
+
+        cols = ",".join([str(i) for i in dataframe.columns.tolist()])
+
+        for i, row in dataframe.iterrows():
+            sql = "INSERT INTO " + table + " (" + cols + ") VALUES (" + "%s," * (len(row) - 1) + "%s)"
+            cursor.execute(sql, tuple(row))
+            self.connection.commit()
+
+        cursor.close()
+
 
 def main():
-    data = ConnexionDatabase('select * from producteurs limit 100')
-    print(data.pandas_sql())
+    data = ConnexionDatabase()
+    df = data.pandas_sql('select * from producteurs limit 100')
+    data.write_database(df, 'producteurs2')
     # print(data.pandas_sql_chunk(10))
 
 
